f822f93bcbaf5b5993e09fca58d160826819dde7
bayard7523 <zheniayakymchuck@gmail.com>
2024-05-26 11:49:46 +0300

    configure spring mvc DispatcherServlet

---
+++ src/main/java/com/bayard7523/kpac/config/WebInitializer.java
@@ -0,0 +1,21 @@
+package com.bayard7523.kpac.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[]{MainConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[]{"/"};
+	}
+}
